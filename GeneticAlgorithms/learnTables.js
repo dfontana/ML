4237a2b4886f173e23a13ef4246bc40aaf730308
dfontana <dyfontana@gmail.com>
2018-05-28 18:40:10 -0400

    adjusted some code from the string GA. Mutation and crossover left.

--- GeneticAlgorithms/learnTables.js
+++ GeneticAlgorithms/learnTables.js
@@ -216,6 +216,7 @@ class GATable {
 
   async evaluate() {
     this.pool.map(l => l.evaluate())
+    this.totalFitness = this.pool.reduce((acc, x) => acc + x.fitness, 0)
   }
 
   async fittest() {
@@ -225,6 +226,87 @@ class GATable {
       }
       return acc;
     }, {fitness: Number.MIN_SAFE_INTEGER})
+  }
+
+  /**
+   * Randomly chooses a genome from the pool based on its proportional fitness.
+   * @return genome, randomly selected
+   */
+  async roulette() {
+    let sum = 0;
+    let threshold = this.totalFitness * Math.random();
+    for(let i = 0; i < this.pool.length; i++) {
+      sum += this.pool[i].fitness
+      if(sum >= threshold) {
+        return this.pool[i];
+      }
+    }
+  }
+
+  /**
+   * Selects (1-P_CROSSOVER)*POOL_SIZE genomes to be carried on into the next generation without any crossover.
+   * @return Array of genomes to be carried into next generation.
+   */
+  async survivors() {
+    let n = (1 - this.cp) * this.ps;
+    return Array.from({length: n}, x => this.roulette())
+  }
+
+  /**
+   * Selects n parents from the given pool for crossover. 
+   * @return Array of objects, holding two genomes each.
+   */
+  async parents() {
+    let n = this.cp * this.ps;
+    return Array.from({length: n}, x => [roulette(), roulette()])
+  }
+
+  /**
+   * TODO
+   * Performs single-point crossover on each of the parental pairs provided, returning a flat list of genomes to include in the next generation.
+   * @param {Array} parents List of objects containing two parents for crossing over.
+   * @return Array of genomes bred from parents.
+   */
+  async crossover(parents) {
+    function cross(pair) {
+      let [p1, p2] = pair;
+      let point = Math.random()*p1.length
+      let c1 = p1.substring(0, point) + p2.substring(point);
+      let c2 = p2.substring(0, point) + p1.substring(point);
+      return [c1, c2]
+    }
+    return parents.reduce((acc, pair) => {
+      acc.push(...cross(pair))
+      return acc
+    }, [])
+  }
+
+  /**
+   * TODO
+   * Performs mutation over the given pool with the given probability.
+   * In this version, mutation is attempted on each gene of each genome within the pool.
+   * of a random genome to mutate.
+   * @return Genome pool after mutation
+   */
+  async mutate() {
+    return pool.map(g => {
+    for(let i = 0; i < g.length; i++) {
+      if(g[i] != goal[i] && Math.random() <= prob) {
+        g = g.substr(0, i) + randChar() + g.substr(i+1);
+      }
+    }
+    return g
+    })
+  }
+
+  async evolve() {
+    let survivors = await this.survivors()
+    let parents = await this.parents()
+    let children = await crossover(parents)
+    this.pool = [...survivors, ...children]
+    await this.mutate()
+    await this.evaluate()
+    await this.fittest()
   }
 }
 
@@ -236,23 +318,6 @@ while(GA.generation < 2000) {
   console.log(`Gen: ${GA.generation} Fit: ${GA.best.fitness}`)
 }
 console.log(`\n Best Layout: ${GA.best.fitness} \n ============ \n ${GA.best.layout.toString()}`)
-
-
-// let generation = 0
-// let pool = initPool(POOL_SIZE, GOAL.length)
-// let evald_pool = fitness(pool, GOAL)
-// let best = fittest(evald_pool)
-while(best.score/GOAL.length < THRESHOLD) {
-  // generation += 1
-  let survivors = selectSurvivors(evald_pool, (1 - P_CROSSOVER) * POOL_SIZE)
-  let parents = selectParents(evald_pool, P_CROSSOVER * POOL_SIZE)
-  let children = crossover(parents)
-  pool = [...survivors, ...children]
-  pool = mutate(pool, P_MUTATION, GOAL)
-  evald_pool = fitness(pool, GOAL)
-  best = fittest(evald_pool)
-  // console.log(`Gen: ${generation} Fit: ${best.score} Str: ${best.g}`)
-}
 
 // ============= People construction
 var getPeople = () => {
@@ -296,83 +361,3 @@ var getPeople = () => {
   
   return Object.values(attendees);
 }
-
-
-// /**
-//  * Randomly chooses a genome from the pool based on its proportional fitness.
-//  * @param {Int} totalFitness Sum of fitness in pool.
-//  * @param {Array} evald_pool Objects representing genome (g) and its fitness (score)
-//  * @return genome string, randomly selected.
-//  */
-// function roulette(totalFitness, evald_pool) {
-//   let sum = 0;
-//   let threshold = totalFitness * Math.random();
-//   for(let i = 0; i < evald_pool.length; i++) {
-//     sum += evald_pool[i].score
-//     if(sum >= threshold) {
-//       return evald_pool[i].g;
-//     }
-//   }
-// }
-
-// /**
-//  * Selects (1-P_CROSSOVER)*POOL_SIZE genomes to be carried on into the next generation without any crossover.
-//  * @param {Array} evald_pool Objects representing genome (g) and its fitness (score)
-//  * @return Array of genome strings to be carried into next generation.
-//  */
-// function selectSurvivors(evald_pool, n) {
-//   let totalFitness = evald_pool.reduce((acc, x) => acc + x.score, 0)
-//   return Array.from({length: n}, x => roulette(totalFitness, evald_pool))
-// }
-
-// /**
-//  * Selects n parents from the given pool for crossover. 
-//  * @param {Array} evald_pool Objects representing genome (g) and its fitness (score)
-//  * @return Array of objects, holding two genomes each.
-//  */
-// function selectParents(evald_pool, n) {
-//   let totalFitness = evald_pool.reduce((acc, x) => acc + x.score, 0)
-//   return Array.from({length: n}, x => [ 
-//     roulette(totalFitness, evald_pool), 
-//     roulette(totalFitness, evald_pool)
-//   ])
-// }
-
-// /**
-//  * Performs single-point crossover on each of the parental pairs provided, returning a flat list of genomes to include in the next generation.
-//  * @param {Array} parents List of objects containing two parents for crossing over.
-//  * @return Array of genomes bred from parents.
-//  */
-// function crossover(parents) {
-//   function cross(pair) {
-//     let [p1, p2] = pair;
-//     let point = Math.random()*p1.length
-//     let c1 = p1.substring(0, point) + p2.substring(point);
-//     let c2 = p2.substring(0, point) + p1.substring(point);
-//     return [c1, c2]
-//   }
-//   return parents.reduce((acc, pair) => {
-//     acc.push(...cross(pair))
-//     return acc
-//   }, [])
-// }
-
-// /**
-//  * Performs mutation over the given pool with the given probability, without mutating values that already meet the goal.
-//  * In this version, mutation is attempted on each gene of each genome within the pool rather than picking a singular gene 
-//  * of a random genome to mutate.
-//  * @param {Array} pool List of genome strings
-//  * @param {float} prob probability of mutation
-//  * @param {String} goal Goal string to achieve
-//  * @return Genome pool after mutation
-//  */
-// function mutate(pool, prob, goal) {
-//   return pool.map(g => {
-// 	for(let i = 0; i < g.length; i++) {
-// 		if(g[i] != goal[i] && Math.random() <= prob) {
-// 		  g = g.substr(0, i) + randChar() + g.substr(i+1);
-// 		}
-// 	}
-// 	return g
-//   })
-// }
